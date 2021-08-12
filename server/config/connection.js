1ce19b198dada669e2178b427b1ebb977d0e18b1
christian louis <clouqb12@yahoo.com>
2021-08-11 20:45:39 -0500

    updated database uri to be the one for the remote db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://dbUser:password@123@cluster0.4m8lm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
